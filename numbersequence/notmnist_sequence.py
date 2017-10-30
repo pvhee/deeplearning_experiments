6089f4e1888cce392bb7c28ffe1674236ada98ed
Peter Vanhee <peter.vanhee@gmail.com>
2017-10-30 10:20:12 +0100

    #1 Finished number sequence generation - cap at 100.000 training examples as pickling doesn't seem to do big files..

--- numbersequence/notmnist_sequence.py
+++ numbersequence/notmnist_sequence.py
@@ -1,15 +1,15 @@
-"""Converts notMNIST data into sequences of letters"""
-import argparse
-import sys
+"""Converts notMNIST data into sequences of numbers"""
 import numpy as np
 from sklearn.utils import shuffle
 from notmnist import notMNIST
 from six.moves import cPickle as pickle
 import os
 
+# Defaults to sequence of 5 numbers
 SEQUENCE_LENGTH = 5
 
-def create_sequence(data, labels, limit=10):
+def create_sequence(data, labels, sequence_length=SEQUENCE_LENGTH, limit=0):
+    '''Create a sequence of numbers from a given number data set'''
     i = 0
     j = 0
 
@@ -24,15 +24,15 @@ def create_sequence(data, labels, limit=10):
 
     while i<limit:
         # check whether we need to restart our loop on a reshuffled data object
-        if j + SEQUENCE_LENGTH >= len(data):
+        if j + sequence_length >= len(data):
             np.random.shuffle(data)
             j=0
 
         # Get a slice into data capturing SEQUENCE_LENGTH images, then concatenate them horizontally
         # Todo: should we add in a separator?
         # Todo we need to add in empty characters too!
-        data_slice = data[j:j+SEQUENCE_LENGTH,]
-        labels_slice = labels[j:j+SEQUENCE_LENGTH,]
+        data_slice = data[j:j+sequence_length,]
+        labels_slice = labels[j:j+sequence_length,]
         img = np.concatenate(data_slice, axis=1)
 
         sequence.append(img)
@@ -40,7 +40,7 @@ def create_sequence(data, labels, limit=10):
 
         # Increase our both counters
         i += 1
-        j += SEQUENCE_LENGTH
+        j += sequence_length
 
     # Convert back to nparrays, which are easier to manage
     sequence = np.asarray(sequence)
@@ -53,76 +53,52 @@ def load_data_or_generate(pickle_seq_file='numbersequence/notMNIST.sequence.pick
     if not os.path.exists(pickle_seq_file) or overwrite:
         print('Generating new sequence pickle from ', pickle_file, ' and saving to ', pickle_seq_file)
         (x_train, y_train), (x_valid, y_valid), (x_test, y_test) = notMNIST.load_data(pickle_file, verbose=0)
-        x2_train, y2_train = create_sequence(x_train, y_train)
+        x2_train, y2_train = create_sequence(x_train, y_train, limit=100000)
         x2_valid, y2_valid = create_sequence(x_valid, y_valid)
         x2_test, y2_test = create_sequence(x_test, y_test)
+
+        print('Generated training set', x2_train.shape, y2_train.shape)
+        print('Generated validation set', x2_valid.shape, y2_valid.shape)
+        print('Generated test set', x2_test.shape, y2_test.shape)
+
         try:
             with open(pickle_seq_file, 'wb') as f:
-                pickle.dump(x2_train, f, pickle.HIGHEST_PROTOCOL)
+                save = {
+                    'train_dataset': x2_train,
+                    'train_labels': y2_train,
+                    'valid_dataset': x2_valid,
+                    'valid_labels': y2_valid,
+                    'test_dataset': x2_test,
+                    'test_labels': y2_test,
+                }
+                pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+                f.close()
         except Exception as e:
             print('Unable to save data to', pickle_seq_file, ':', e)
+            return
     else:
         print('Loading existing sequence pickle from ', pickle_seq_file)
+        try:
+            with open(pickle_seq_file, 'rb') as f:
+                save = pickle.load(f)
+                x2_train = save['train_dataset']
+                y2_train = save['train_labels']
+                x2_valid = save['valid_dataset']
+                y2_valid = save['valid_labels']
+                x2_test = save['test_dataset']
+                y2_test = save['test_labels']
+                del save  # hint to help gc free up memory
+        except Exception as e:
+            print('Unable to read data from', pickle_seq_file, ':', e)
+            return
 
     if verbose:
         print('Training set', x2_train.shape, y2_train.shape)
         print('Validation set', x2_valid.shape, y2_valid.shape)
         print('Test set', x2_test.shape, y2_test.shape)
 
-
     return (x2_train, y2_train), (x2_valid, y2_valid), (x2_test, y2_test)
 
-# print(labels)
-
-# print(labels.argmax(axis=-1))
-
-
 # Visualise some examples
-# notMNIST.visualize_batch(seq[0:8:], labels[0:8:].argmax(axis=-1))
-
-
-load_data_or_generate()
-
-# Build a maybe construct to build the sequence from scratch if not found...
-
-
-# def maybe_download(filename, expected_bytes):
-#   """Download a file if not present, and make sure it's the right size."""
-#   if not os.path.exists(filename):
-#     filename, _ = urllib.request.urlretrieve(url + filename, filename)
-#   statinfo = os.stat(filename)
-#   if statinfo.st_size == expected_bytes:
-#     print('Found and verified', filename)
-#   else:
-#     print(statinfo.st_size)
-#     raise Exception(
-#         'Failed to verify ' + filename + '. Can you get to it with a browser?')
-#   return filename
-
-
-
-
-# def main(unused_argv):
-#     train_dataset, train_labels, valid_dataset, valid_labels, test_dataset, test_labels = convert_to_records.read_pickle(FLAGS.input_file)
-#
-#     _test_dataset, _test_labels = create_sequence(test_dataset, test_labels, 16)
-#     convert_to_records.convert_to(_test_dataset, _test_labels, FLAGS.directory, 'test.sequence')
-#
-#
-# if __name__ == '__main__':
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument(
-#         '--input-file',
-#         type=str,
-#         default='notMNIST.pickle',
-#         help='Pickle file with not MNIST data'
-#     )
-#     parser.add_argument(
-#         '--directory',
-#         type=str,
-#         default='/tmp/data/',
-#         help='Directory to write the converted result'
-#     )
-#
-#     FLAGS, unparsed = parser.parse_known_args()
-#     tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+(x2_train, y2_train), (x2_valid, y2_valid), (x2_test, y2_test) = load_data_or_generate(verbose=1, overwrite=0)
+notMNIST.visualize_batch(x2_train[0:8:], y2_train[0:8:].argmax(axis=-1))
