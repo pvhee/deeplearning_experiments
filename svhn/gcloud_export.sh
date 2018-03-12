55361513bf2c3c98cba3bae3360e11cff1c13537
pvhee <peter.vanhee@gmail.com>
2018-03-12 13:06:11 +0000

    #3 bash script to export model to gcloud, creating bucket, model & version

---
+++ svhn/gcloud_export.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BUCKET='svhn-digits-ml'
+PROJECT='svhn-digits'
+REGION='us-central1'
+MODEL_NAME='svhn_digits'
+MODEL_VERSION='default_version'
+EXPORT_VERSION='1520853564'
+EXPORT_DIR='export/'$EXPORT_VERSION
+
+# Set default project and region
+gcloud config set project $PROJECT
+gcloud config set compute/region $REGION
+
+# Create bucket
+if ! gsutil ls | grep -q gs://${BUCKET}/; then
+  gsutil mb -l ${REGION} gs://${BUCKET}
+fi
+
+# Upload model to bucket
+# Use -n (no-clobber) to not overwrite existing files
+gsutil cp -n -a public-read -R ${EXPORT_DIR} gs://${BUCKET}
+
+# Clean out bucket if you want to upload a new version
+#gsutil rm gs://${BUCKET}/**
+
+# Fetch the model location
+MODEL_LOCATION=$(gsutil ls gs://${BUCKET}/ | tail -1)
+echo "Using model location: ${MODEL_LOCATION}"
+
+# Create model in Cloud ML
+gcloud ml-engine models create ${MODEL_NAME} --regions $REGION
+gcloud ml-engine versions create ${MODEL_VERSION} --model ${MODEL_NAME} --origin ${MODEL_LOCATION}
+
+# Get info about this model & version
+gcloud ml-engine versions describe ${MODEL_VERSION} --model ${MODEL_NAME}
