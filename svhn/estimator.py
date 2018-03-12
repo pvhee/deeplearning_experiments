27abe64ef3f3ebabddc07780e93db26e041bdf3c
pvhee <peter.vanhee@gmail.com>
2018-03-12 11:15:20 +0000

    #3 add export function to export esimator for use in google cloud. Needs TF 1.5 or above due to a bug in exporting from a keras model

--- svhn/estimator.py
+++ svhn/estimator.py
@@ -5,6 +5,12 @@ See https://cloud.google.com/blog/big-data/2017/12/new-in-tensorflow-14-converti
 import tensorflow as tf
 import numpy as np
 from predict import load_images
+from model import INPUT_SHAPE
+from tensorflow.python.estimator.export.export import build_raw_serving_input_receiver_fn
+
+# Input tensor name matching our input layer
+# @todo give this a custom name rather than having Keras name this
+INPUT_TENSOR_NAME = "input_1"
 
 def convert_to_estimator(model_file):
     """Convert saved Keras model to TF Estimator
@@ -13,13 +19,19 @@ def convert_to_estimator(model_file):
     return estimator_model
 
 def input_function(features, labels=None, shuffle=False):
+    """Returns an input function that can be used for prediction within an estimator"""
     # @todo rename input_1
     input_fn = tf.estimator.inputs.numpy_input_fn(
-        x={"input_1": features},
+        x={INPUT_TENSOR_NAME: features},
         y=labels,
         shuffle=shuffle
     )
     return input_fn
+
+def serving_input_function():
+    """Returns a serving input function used for exporting a model"""
+    feature_spec = {INPUT_TENSOR_NAME: tf.FixedLenFeature(dtype=tf.float32, shape=INPUT_SHAPE)}
+    return tf.estimator.export.build_parsing_serving_input_receiver_fn(feature_spec)()
 
 def infer(argv=None):
     """Run the inference and print the results to stdout."""
@@ -34,6 +46,13 @@ def infer(argv=None):
         predicted_label = np.argmax(p.values(), axis=-1)
         print predicted_label
 
+def export():
+    """Export estimator for use in Google Cloud ML"""
+    estimator = convert_to_estimator('svhn_model.h5')
+    export = estimator.export_savedmodel(export_dir_base='.', serving_input_receiver_fn=serving_input_function)
+    print export
+
 ## Run script
 if __name__ == "__main__":
-    infer()
+    # infer()
+    export()
